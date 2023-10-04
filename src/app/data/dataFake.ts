83010f304a7670314c6e8c3ddcb6b1e9247d68b3
gabi <gabi2000_jb@hotmail.com>
2023-10-04 13:58:45 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "CAPITÃO AMÉRICA RETORNARÁ?",
+        "descripition": "Marvel Studios anuncia que Capitão América estará em novo filme da franquia.",
+        "photoCover":"https://olhardigital.com.br/wp-content/uploads/2021/03/Chris-Evans-como-Capitao-America-scaled.jpg"
+    },
+    {
+        "id":"2",
+        "title": "Nova série anunciada no Disney +",
+        "descripition": "bla bla bla",
+        "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    },
+    {
+        "id":"3",
+        "title": "Pantera Negra será homenageado no próximo filme dos Vingadores",
+        "descripition": "bla bla bla",
+        "photoCover":"https://img.elo7.com.br/product/original/37EBEF7/painel-1x65cm-pantera-negra-wakanda-festa-expressa.jpg"
+    },
+    {
+        "id":"4",
+        "title": "Robert Downey Jr não será mais o Homem de Ferro",
+        "descripition": "bla bla bla",
+        "photoCover":"https://cinepop.com.br/wp-content/uploads/2022/09/homem-de-ferro.jpg"
+    }
+]
